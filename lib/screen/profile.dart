953243f3e7c4e20c30276d79dd08c061fd6fb6f8
= <gianni.giudice@lacatholille.fr>
2022-01-29 16:09:40 +0100

    :sparkles: AJout de la déconnexion

--- lib/screen/profile.dart
+++ lib/screen/profile.dart
@@ -3,6 +3,8 @@ import 'dart:developer';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:tchintchin/screen/homepage.dart';
+
 
 class Profile extends StatefulWidget {
   const Profile({Key? key}) : super(key: key);
@@ -26,10 +28,42 @@ class _ProfileState extends State<Profile> {
   Widget build(BuildContext context) {
     init();
     return Scaffold(
-      body: Column(children: [
-        Text('Bienvenue ' + (user != null ? user!.email! : 'dommage'))
-      ]),
-    );
+        body: Container(
+            padding: EdgeInsets.all(15),
+            child:
+                Column(mainAxisAlignment: MainAxisAlignment.start, children: [
+              Align(
+                  alignment: Alignment.centerLeft,
+                  child: Container(
+                      margin: EdgeInsets.only(bottom: 15),
+                      child: Text(
+                        'Bienvenue ' +
+                            (user != null ? user!.email! : '') +
+                            ' !',
+                        textAlign: TextAlign.left,
+                      ))),
+              Align(
+                  alignment: Alignment.centerLeft,
+                  child: Container(
+                      child: Text('ID utilisateur : ' +
+                          (user != null ? user!.uid : '')))),
+              Container(
+                margin: EdgeInsets.only(top: 10),
+                child:
+              ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      minimumSize:
+                          Size(MediaQuery.of(context).size.width * 0.65, 50),
+                      primary: const Color(0xffCC4B29),
+                      onPrimary: const Color(0xffffffff)),
+                  child: Text('Déconnexion'),
+                  onPressed: () async {
+                    auth != null ? await auth!.signOut() : null;
+                    Navigator.of(context).push(
+                        MaterialPageRoute(
+                            builder: (context) =>
+                                MyHomePage(title: 'Tchin Tchin')));
+                  }))
+            ])));
   }
-
 }
